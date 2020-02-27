59562dec534c03a0fe39f92b3b9fbac0e9bca778
apzheng123 <48306930+apzheng123@users.noreply.github.com>
2020-02-27 19:30:03 +0800

    Update Pure_Pursuit.py

--- Pure_Pursuit.py
+++ Pure_Pursuit.py
@@ -1,13 +1,11 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
-k = 0.1  # 前视距离系数
+k = 0.2 # 前视距离系数
 Lfc = 2.0  # 前视距离
 Kp = 1.0  # 速度P控制器系数
 dt = 0.1  # 时间间隔，单位：s
 L = 2.9 
-
 class VehicleState:
  
     def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
@@ -25,7 +23,6 @@ def update(state, a, delta):
     state.v = state.v + a * dt
  
     return state
-
 def PControl(target, current):
     a = Kp * (target - current)
  
@@ -57,7 +54,6 @@ def pure_pursuit_control(state, cx, cy, pind):
     delta = math.atan2(2.0 * L * math.sin(alpha) / Lf, 1.0)
  
     return delta, ind
-
 def calc_target_index(state, cx, cy):
     # 搜索最临近的路点
     dx = [state.x - icx for icx in cx]
@@ -69,30 +65,32 @@ def calc_target_index(state, cx, cy):
     Lf = k * state.v + Lfc
  
     while Lf > L and (ind + 1) < len(cx):
-        dx = cx[ind + 1] - cx[ind]
-        dy = cy[ind + 1] - cy[ind]
-        L += math.sqrt(dx ** 2 + dy ** 2)
+       #dx = cx[ind + 1] - cx[ind]
+       #dy = cy[ind + 1] - cy[ind]
+        dx = state.x - cx[ind ]
+        dy = state.y - cy[ind ]
+        L = math.sqrt(dx ** 2 + dy ** 2)
         ind += 1
  
     return ind
-
-
     #  设置目标路点
-cx =  np.arange(0, 100, 1)
+cx =  np.arange(0, 60, 1)
 #cy = [math.sin(ix / 5.0) * ix / 2.0 for ix in cx]
 dx1 = 25
 dx2 = 21.95
 dy1 = 4.05
 dy2 = 5.7
-z1 = 2.4 / dx1 * (cx - 27.19) - 1.2
-z2 = 2.4 / dx2 * (cx - 56.46) - 1.2
+z1 = 2.4 / dx1 * (2 * cx - 27.19) - 1.2
+z2 = 2.4 / dx2 * (2 * cx - 56.46) - 1.2
 cy = dy1 / 2 * (1 + np.tanh(z1)) - dy2 / 2 * (1 + np.tanh(z2))
+
+
 target_speed = 60 / 3.6  # [m/s]
 
 T = 100.0  # 最大模拟时间
 
 # 设置车辆的初始状态
-state = VehicleState(x=-0.0, y=-2.0, yaw=0.0, v=0.0)
+state = VehicleState(x=-0.0, y=0.0, yaw=0.0, v= 0.0)
 
 lastIndex = len(cx) - 1
 time = 0.0
@@ -120,9 +118,23 @@ while T >= time and lastIndex > target_ind:
     plt.plot(cx, cy, ".r", label="course")
     plt.plot(x, y, "-b", label="trajectory")
     plt.plot(cx[target_ind], cy[target_ind], "go", label="target")
-    plt.axis([0,100,-5,5])
+    plt.axis([0,60,-5,5])
     plt.grid(True)
     plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
     plt.pause(0.001)
  
-    
+
+plt.plot(cx, cy, ".r", label="course")
+plt.plot(x, y, "-b", label="trajectory")
+plt.legend()
+plt.xlabel("x[m]")
+plt.ylabel("y[m]")
+plt.axis([0,80,-5,5])
+plt.grid(True)
+
+plt.subplots(1)
+plt.plot(t, [iv * 3.6 for iv in v], "-r")
+plt.xlabel("Time[s]")
+plt.ylabel("Speed[km/h]")
+plt.grid(True)
+plt.show()
